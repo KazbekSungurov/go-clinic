22126293455bbbd3269f81b14972ed7b410903f0
Kazbek Sungurov <kazbeksungurov@gmail.com>
2023-01-30 22:55:35 +0300

    schedule operation build added

--- clinic-api/go.mod
+++ clinic-api/go.mod
@@ -1,5 +1,11 @@
 module clinic-api
 
-go 1.17
+go 1.18
 
-require github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
+require github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/exp v0.0.0-20230129154200-a960b3787bd2 // indirect
+)
--- clinic-api/service/department.go
+++ clinic-api/service/department.go
@@ -2,7 +2,14 @@ package service
 
 import (
 	"clinic-api/models"
+	"errors"
 	validation "github.com/go-ozzo/ozzo-validation"
+	"golang.org/x/exp/slices"
+	"time"
+)
+
+var (
+	errTimeIsNil = errors.New("заданные промежутки времени некорректны")
 )
 
 type department struct {
@@ -35,3 +42,48 @@ func (d *department) Get(id uint) (m *models.Department, err error) {
 	//TODO implement me
 	panic("implement me")
 }
+
+func (d *department) ScheduleOperationBuild(m *models.Department, from, to time.Time) error {
+	if from.IsZero() || to.IsZero() {
+		return errTimeIsNil
+	}
+
+	if from.After(to) {
+		from, to = to, from
+	}
+
+	for i := from; i.Before(to); i.AddDate(0, 0, 1) {
+		if slices.Contains[time.Weekday](m.WeekDays, i.Weekday()) {
+			m.Appointments = append(m.Appointments, models.Appointment{Day: i})
+			break
+		}
+	}
+
+	return nil
+}
+
+//func (d *department) ScheduleConsultBuild(m *models.Department, from, to time.Time) error {
+//	if from.IsZero() || to.IsZero() {
+//		return errTimeIsNil
+//	}
+//
+//	if from.After(to) {
+//		from, to = to, from
+//	}
+//
+//	for i := from; i.Before(to); i.AddDate(0, 0, 1) {
+//		if slices.Contains[time.Weekday](m.WeekDays, i.Weekday()) {
+//			var as []models.Appointment
+//			m.ScheduleDetails.StartAt
+//			m.ScheduleDetails.EndAt
+//			m.ScheduleDetails.BreakStart
+//			m.ScheduleDetails.BreakDuration
+//			m.ScheduleDetails.ExaminationDuration
+//			as = append(as)
+//			m.Appointments = append(m.Appointments, models.Appointment{Day: i})
+//			break
+//		}
+//	}
+//
+//	return nil
+//}
